7e53c7f5d3a22f3d6b15d7392901b130d880e5e3
Loui <asural@vip.qq.com>
2017-06-03 18:15:49 +0800

    44

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,40 @@
 var gulp = require('gulp');
 gulp.task('default', function() {
     console.log('Hello Gulp!');
-})
+});
+var tslint = require('gulp-tslint');
+
+gulp.task("lint", () =>
+    gulp.src(['./source/ts/**/**.ts', , './test/**/**.test.ts'])
+    .pipe(tslint({
+        formatter: "verbose"
+    }))
+    .pipe(tslint.report())
+);
+
+var ts = require('gulp-typescript');
+var tsProject = ts.createProject({
+    removeComments: true,
+    noImplicitAny: true,
+    target: 'ES3',
+    module: 'commonjs',
+    declarationFiles: false
+});
+gulp.task('tsc', function() {
+    return gulp.src('./source/ts/**/**.ts')
+        .pipe(ts(tsProject))
+        .js.pipe(gulp.dest('./temp/source/js'));
+});
+var tsTestProject = ts.createProject({
+    removeComments: true,
+    noImplicitAny: true,
+    target: 'ES3',
+    module: 'commonjs',
+    declarationFiles: false
+});
+gulp.task('tsc-tests', function() {
+    return gulp.src('./test/ts/**/**.test.ts')
+        .pipe(ts(tsTestProject))
+        .js.pipe(gulp.dest('./temp/test/'));
+});
+gulp.task('default', ['lint', 'tsc', 'tsc-tests']);
